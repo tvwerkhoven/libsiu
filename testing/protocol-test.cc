3237a2abb41501c7db81bb4dff2b15f2c5c4bc96
Tim van Werkhoven <t.i.m.vanwerkhoven@xs4all.nl>
2011-01-04 15:48:59 +0100

    Improved test, returns 0 or -1 depending on succes or failure.

--- testing/protocol-test.cc
+++ testing/protocol-test.cc
@@ -13,11 +13,21 @@ void on_connect(Connection *connection, bool status);
 
 static void on_client_msg(std::string line);
 
+int retval=0;
+string n1 = "SYS";
+string n2 = "WFS1";
+
+string msg1 = "SYS hello world";
+string msg2 = "WFS1 hello sky!";
+
+string resp = "RECEIVE OK";
+
 int main() {
 	string msg;
 	
-	Protocol::Server serv1("1234", "SYS");
-	Protocol::Server serv2("1234", "WFS1");
+	// Start two servers
+	Protocol::Server serv1("1234", n1);
+	Protocol::Server serv2("1234", n2);
 
 	serv1.slot_message = sigc::ptr_fun(on_message);
 	serv1.slot_connected = sigc::ptr_fun(on_connect);
@@ -33,35 +43,47 @@ int main() {
 	client.slot_message = sigc::ptr_fun(on_client_msg);
 	sleep(1);
 	
-	msg = "SYS hello world";
-	fprintf(stderr, "client.write(\"%s\");\n", msg.c_str());
-	client.write(msg);
+	fprintf(stderr, "client.write(\"%s\");\n", msg1.c_str());
+	client.write(msg1);
 
-	msg = "WFS1 hello sky!";
-	fprintf(stderr, "client.write(\"%s\");\n", msg.c_str());
-	client.write(msg);
+	fprintf(stderr, "client.write(\"%s\");\n", msg2.c_str());
+	client.write(msg2);
 	
-	//while (1)
+	sleep(1);
 	
-	sleep(2);
+	if (retval == 0)
+		fprintf(stderr, "protocol-test.cc SUCCESS!\n");
+	else
+		fprintf(stderr, "protocol-test.cc FAILED!\n");
 
-	return 0;
+	return retval;
 }
 
 void on_connect(Connection *connection, bool status) {
-	fprintf(stderr, "serv:on_connected: %d\n", status);
+	fprintf(stderr, "%s:on_connected: %d\n", connection->server->name.c_str(), status);
 }
 
 void on_message(Connection *connection, string line) {
 	fprintf(stderr, "%s:on_message: %s\n", connection->server->name.c_str(), line.c_str());
+
+	if ((connection->server->name == n1 && line != msg1) ||
+			(connection->server->name == n2 && line != msg2)) {
+		fprintf(stderr, "%s:on_message: ERROR!\n", connection->server->name.c_str());
+		retval = -1;
+	}
+	
 	string word;
 	while ((word = popword(line)).length())
-		fprintf(stderr, "serv:on_message: %s\n", word.c_str());
+		fprintf(stderr, "%s:on_message: %s\n", connection->server->name.c_str(), word.c_str());
 	
-	fprintf(stderr, "writing\n");
-	connection->write("OK, got it");
+	fprintf(stderr, "%s:on_message: writing response\n", connection->server->name.c_str());
+	connection->write(resp);
 }
 
 void on_client_msg(std::string line) {
 	fprintf(stderr, "cli:on_client_msg: %s\n", line.c_str());
+	if (line != resp) {
+		fprintf(stderr, "cli:on_client_msg: ERROR!\n");
+		retval=-1;
+	}
 }
