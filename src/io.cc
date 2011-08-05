7ec5ab38d9a4a6c06a8dbb56ac427134196cc8d2
Tim van Werkhoven <t.i.m.vanwerkhoven@xs4all.nl>
2011-08-05 11:15:52 +0200

    More const, more docs, no format.h dependency
    Applied more constness in functions when applicable. Improved code
    description somewhat and incorporated some format.h code (vformat) to remove
    the dependency on this file.

--- src/io.cc
+++ src/io.cc
@@ -46,27 +46,30 @@ Io::~Io(void) {
 		fclose(logfd);
 }
 
-int Io::setLogfile(Path &file) {
+int Io::setLogfile(const Path &file) {
 	logfile = file;
 	logfd = fopen(logfile.c_str(), "a");
-	if (!logfd) return -1;
+	if (!logfd)
+		return -1;
 	return 0;
 }
 
-int Io::msg(int type, const std::string message) const {
+int Io::msg(const int type, const std::string message) const {
 	// Apply default mask
-	type = (type | defmask);
+	int mytype = (type | defmask);
 	
 	// Separate level from type mask	
-	int level = type & IO_LEVEL_MASK;
+	int level = mytype & IO_LEVEL_MASK;
 
 	if (level <= verb) {
 		std::string tmpmsg;
 
-		// Build prefix
-		if (!(type & IO_NOID))
+		// Build prefix unless IO_NOID is set
+		if (!(mytype & IO_NOID))
 			tmpmsg += "[" + PREFIX[level] + "] ";
-		if (type & IO_THR) {
+
+		// Add thread ID if IO_THR is set
+		if (mytype & IO_THR) {
 			// From: http://stackoverflow.com/questions/1759794/how-to-print-pthread-t
 			pthread_t pt = pthread_self();
 			unsigned char *ptc = (unsigned char*)(void*)(&pt);
@@ -76,39 +79,42 @@ int Io::msg(int type, const std::string message) const {
 			tmpmsg += ")";
 		}
 		
-		// Add message
+		// Add message to prefix
 		tmpmsg = tmpmsg + message;
 		
-		// Add postfix
-		if (!(type & IO_NOLF))
+		// Add postfix unless IO_NOLF is set
+		if (!(mytype & IO_NOLF))
 			tmpmsg += "\n";
-
+		
+		// Print this to requested term & flush
 		fputs(tmpmsg.c_str(), termfd);
 		fflush(termfd);
 		
+		// If we're logging to a file, check this as well
 		if (logfd) {
 			fputs(tmpmsg.c_str(), logfd);
 			fflush(logfd);
 		}
 	}
 	
-	if (type & (IO_FATAL)) exit(-1);
-	if (type & (IO_ERR)) return -1;
+	if (mytype & (IO_FATAL)) exit(-1);
+	if (mytype & (IO_ERR)) return -1;
 	
 	return 0;
 }
 
-int Io::msg(int type, const char *fmtstr, ...) const {
+int Io::msg(const int type, const char *fmtstr, ...) const {
 	// Separate level from type mask
 	int level = type & IO_LEVEL_MASK;
 	
 	if (level <= verb) {
 		va_list va;
 		va_start(va, fmtstr);
-		std::string result = vformat(fmtstr, va);
+		char buf[1024];
+		vsnprintf(buf, sizeof buf, fmtstr, va);
 		va_end(va);
 	
-		msg(type, result);
+		msg(type, std::string(buf));
 	}
 
 	if (type & IO_FATAL) exit(-1);
